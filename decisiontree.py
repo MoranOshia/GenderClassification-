3965ff3e1336a5a09d084602e1000fc2974f338a
MoranOshia <44996337+MoranOshia@users.noreply.github.com>
2021-04-20 13:53:39 +0300

    Delete decisiontree.py

--- decisiontree.py
+++
@@ -1,7 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-
-def dt(x_train, x_test, y_train, y_test):
-
-    dt=DecisionTreeClassifier()
-    dt.fit(x_train,y_train)
-    return dt.score(x_test,y_test) * 100
